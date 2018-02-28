6ce826ec3da6081e9f6873837fd59a8e914f2a41
hynd08 <hamidi.hynd@gmail.com>
2018-02-28 16:43:29 +0100

    premier commentaire du blog Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
-
+import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {Article} from './article';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title: string;
+    articles: Array<Article>;
+    editing: boolean;
+    editArticle: Article;
+
+    constructor() {
+        this.editing = false;
+        this.editArticle = new Article(0, '');
+        this.title = ' Mon super Blog';
+        this.articles = new Array();
+        // this.articles.push(new Article(99, 'Article de test', 'super description'));
+    }
+    addArticle() {
+        this.editing = true;
+    }
+    backToList() {
+        this.editing = false;
+    }
+    saveArticle(myForm: NgForm) {
+        this.articles.push(JSON.parse(JSON.stringify(this.editArticle)));
+        myForm.resetForm();
+
+
+    }
 }
